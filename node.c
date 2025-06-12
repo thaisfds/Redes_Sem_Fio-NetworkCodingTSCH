6c0a2b55ccf0cc395b85fdde2e2c32d39fcf29b2
Thaís Ferreira <thaisfdasilva159@gmail.com>
2025-06-12 17:32:41 -0300

    adding comments

--- node.c
+++ node.c
@@ -1,94 +1,64 @@
-/*
- * Copyright (c) 2015, SICS Swedish ICT.
- * Copyright (c) 2018, University of Bristol - http://www.bristol.ac.uk
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- */
-/**
- * \file
- *         A RPL+TSCH node.
- *
- * \author Simon Duquennoy <simonduq@sics.se>
- *         Atis Elsts <atis.elsts@bristol.ac.uk>
- */
+#include "contiki.h"
+#include "sys/node-id.h"
+#include "sys/log.h"
+#include "net/ipv6/uip-ds6-route.h"
+#include "net/ipv6/uip-sr.h"
+#include "net/mac/tsch/tsch.h"
+#include "net/routing/routing.h"
 
- #include "contiki.h"
- #include "sys/node-id.h"
- #include "sys/log.h"
- #include "net/ipv6/uip-ds6-route.h"
- #include "net/ipv6/uip-sr.h"
- #include "net/mac/tsch/tsch.h"
- #include "net/routing/routing.h"
- 
- #define DEBUG DEBUG_PRINT
- #include "net/ipv6/uip-debug.h"
- 
- /*---------------------------------------------------------------------------*/
- PROCESS(node_process, "RPL Node");
- AUTOSTART_PROCESSES(&node_process);
- 
- /*---------------------------------------------------------------------------*/
- PROCESS_THREAD(node_process, ev, data)
- {
-   int is_coordinator;
- 
-   PROCESS_BEGIN();
- 
-   is_coordinator = 0;
- 
- #if CONTIKI_TARGET_COOJA
-   is_coordinator = (node_id == 1);
- #endif
- 
-   if(is_coordinator) {
-     NETSTACK_ROUTING.root_start();
-   }
-   NETSTACK_MAC.on();
- 
- #if WITH_PERIODIC_ROUTES_PRINT
-   {
-     static struct etimer et;
-     /* Print out routing tables every minute */
-     etimer_set(&et, CLOCK_SECOND * 60);
-     while(1) {
-       /* Used for non-regression testing */
-       #if (UIP_MAX_ROUTES != 0)
-         PRINTF("Routing entries: %u\n", uip_ds6_route_num_routes());
-       #endif
-       #if (UIP_SR_LINK_NUM != 0)
-         PRINTF("Routing links: %u\n", uip_sr_num_nodes());
-       #endif
-       PROCESS_YIELD_UNTIL(etimer_expired(&et));
-       etimer_reset(&et);
-     }
-   }
- #endif /* WITH_PERIODIC_ROUTES_PRINT */
- 
-   PROCESS_END();
- }
- /*---------------------------------------------------------------------------*/
- 
+#define DEBUG DEBUG_PRINT
+#include "net/ipv6/uip-debug.h"
+
+/* Define o processo chamado "RPL Node" */
+PROCESS(node_process, "RPL Node");
+
+/* Define que este processo será iniciado automaticamente ao ligar o nó */
+AUTOSTART_PROCESSES(&node_process);
+
+/* Corpo do processo */
+PROCESS_THREAD(node_process, ev, data)
+{
+  int is_coordinator; // Flag para verificar se este nó é o coordenador (root)
+
+  PROCESS_BEGIN(); // Início do processo Contiki
+
+  is_coordinator = 0; // Inicialmente assume que não é coordenador
+
+  /* Se estiver rodando no simulador Cooja, define o nó 1 como coordenador */
+#if CONTIKI_TARGET_COOJA
+  is_coordinator = (node_id == 1); // node_id é definido automaticamente no Cooja
+#endif
+
+  /* Se este nó for o coordenador, inicia o roteador RPL como raiz */
+  if(is_coordinator) {
+    NETSTACK_ROUTING.root_start(); // Este nó vira o root da árvore RPL (DODAG)
+  }
+
+  /* Liga a camada MAC (nesse caso, TSCH), que não inicia automaticamente */
+  NETSTACK_MAC.on();
+
+  /* Se a flag WITH_PERIODIC_ROUTES_PRINT estiver definida,
+     imprime a tabela de roteamento a cada minuto */
+#if WITH_PERIODIC_ROUTES_PRINT
+  {
+    static struct etimer et; // Timer para controlar o intervalo de impressão
+
+    etimer_set(&et, CLOCK_SECOND * 60); // Define o timer para 60 segundos
+
+    while(1) {
+      /* Imprime o número de entradas na tabela de rotas */
+#if (UIP_MAX_ROUTES != 0)
+      PRINTF("Routing entries: %u\n", uip_ds6_route_num_routes());
+#endif
+      /* Imprime o número de nós na topologia RPL (quando usa Source Routing) */
+#if (UIP_SR_LINK_NUM != 0)
+      PRINTF("Routing links: %u\n", uip_sr_num_nodes());
+#endif
+      PROCESS_YIELD_UNTIL(etimer_expired(&et)); // Espera até que o timer expire
+      etimer_reset(&et); // Reinicia o timer
+    }
+  }
+#endif /* WITH_PERIODIC_ROUTES_PRINT */
+
+  PROCESS_END(); // Fim do processo Contiki
+}
